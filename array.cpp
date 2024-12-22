7f6e28b8087d1890979d0299f399d04b4e6d25fd
Bobrkurva91 <tb080491@gmail.com>
2024-12-22 21:31:45 +0300

    Реализованы функции в array.cpp

---
+++ array.cpp
@@ -0,0 +1,29 @@
+#include "array.h"
+
+int max(int left, int right) {
+    return (left > right) ? left : right;
+}
+
+int min(int left, int right) {
+    return (left < right) ? left : right;
+}
+
+int min_index(int* massive, int size) {
+    int min_idx = 0;
+    for (int i = 1; i < size; i++) {
+        if (massive[i] < massive[min_idx]) {
+            min_idx = i;
+        }
+    }
+    return min_idx;
+}
+
+int max_index(int* massive, int size) {
+    int max_idx = 0;
+    for (int i = 1; i < size; i++) {
+        if (massive[i] > massive[max_idx]) {
+            max_idx = i;
+        }
+    }
+    return max_idx;
+}
